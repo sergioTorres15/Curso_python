4ac21b6e39b776f176d99ea468c2a3dfe0e7441e
sergi <torrresloezapablosergio@gmail.com>
2019-06-27 17:56:22 -0500

    añadiendo conversor de grados

---
+++ conversion_grados-farengeit.py
@@ -0,0 +1,8 @@
+print("bienvenido al conversor fahrenheit")
+print("grados farengeit a celsius")
+
+grados_fahrenheit = float(input("inserta grados fahrenheit --> :"))
+
+grados_celsius = (grados_fahrenheit -32) / 1.8
+
+print("Los grados en celsius son : {}".format(grados_celsius))
