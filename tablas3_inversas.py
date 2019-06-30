8366609206ee607d171b0edb0e38878f9b3ea494
sergi <torrresloezapablosergio@gmail.com>
2019-06-30 18:01:33 -0500

    añadiendo tablas de multiplicar inversas

---
+++ tablas3_inversas.py
@@ -0,0 +1,8 @@
+numero_tabla = int(input("de que num. quieres la tabla de multiplicar? :"))
+
+
+for multiplo in range(10,0,-1):
+
+        print("{}x{}={}".format(numero_tabla,multiplo,numero_tabla*multiplo))
+
+
