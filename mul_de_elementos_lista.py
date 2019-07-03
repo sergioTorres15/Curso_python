6c7fe783ee920a68714fc078a4bd901b18729ac3
sergi <torrresloezapablosergio@gmail.com>
2019-07-03 09:18:01 -0500

    añadiendo multiplicacion de elementos en una lista sin ciclo for

--- mul_de_elementos_lista.py
+++ mul_de_elementos_lista.py
@@ -1,10 +1,9 @@
 lista_numeros = [1,2,3,4,5]
 lista =[]
 
-for dato in lista_numeros:
-    import operator
-    import functools
+import operator
+import functools
 
-    resultado = functools.reduce(operator.mul, [1, 2, 3, 4, 5,], 1)
+resultado = functools.reduce(operator.mul, [1, 2, 3, 4, 5,], 1)
 
 print(resultado)
