27889dc52a543cb1d5ac24e4113f3f57ce537e06
sergi <torrresloezapablosergio@gmail.com>
2019-07-07 10:36:09 -0500

    añadiendo fizzbuzz 3 y 5 con bazinga

---
+++ fizzbuzz_bazinga.py
@@ -0,0 +1,18 @@
+
+numeros = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,20,15,30,60,70]
+
+for indice in range(len(numeros)):
+    numero = numeros[indice]
+
+
+    if numero % 3 == 0 and numero % 5 == 0:
+            numeros[indice] = "bazinga"
+
+    elif numero % 3 == 0 or numero % 5 == 0:
+        numeros[indice] = ""
+
+        if numero % 3 == 0:
+            numeros[indice] += "Fizz"
+        elif numero % 5 == 0:
+            numeros[indice] += "Buzz"
+print(numeros)
