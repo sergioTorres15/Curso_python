044ec972d22a606980d16f3073bba0209b1ba904
sergi <torrresloezapablosergio@gmail.com>
2019-07-12 20:25:35 -0500

    añadiendo datetime five days

---
+++ fecha_five_days.py
@@ -0,0 +1,7 @@
+import  datetime
+
+
+fecha_ahora = datetime.datetime.now()
+cinco = fecha_ahora - datetime.timedelta(days=5)
+
+print("la fecha de hace 5 dias fue el  {}".format(cinco.strftime("%d del %m del año %Y ")))
